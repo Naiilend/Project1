39fd5f620263c0271d5866de5cb02dc012aabfd1
Naiilend <147986740+Naiilend@users.noreply.github.com>
2023-10-19 18:42:54 +0500

    ProjectNailend

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, };
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);    
+        }
+
+
+
+
+
+
+    }
+}
+
+    
+
